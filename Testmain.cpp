9199b2c0fe69d29e5f48eb4186f2266e3a3df37d
Theprocyon <46133206+Theprocyon@users.noreply.github.com>
2020-12-26 01:38:24 +0900

    Update Testmain.cpp

--- Testmain.cpp
+++ Testmain.cpp
@@ -1,8 +1,12 @@
 #include "UART.h"
+#include <iostream>
+using namespace std;
 
 int main(){
-	Uart Test("/dev/ttyTHS1");
-	unsigned char message = 1;
-	Test.sendUart(&message);
+	Uart Test("/dev/pts/4");
+	Test.sendUart('x');
+	int x;
+	cin >> x;
+	Test.readUart();
 	return 0;
 }
