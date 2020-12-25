b51c7a4e21ff14fbae21d65e64e281b4f050a854
Theprocyon <46133206+Theprocyon@users.noreply.github.com>
2020-12-25 16:44:45 +0900

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,17 +1,17 @@
 ﻿#ifndef _UART_H
 #define _UART_H
 
-const char* uart_target = "/dev/ttyTHS1";
 #define NSERIAL   256
 #define BAUDRATE       B57600 //50ms
 #define BUFFERSIZE     50
 
 class Uart {
 private:
+	const char * uart_target;
 	int Port;
 public:
 	unsigned char rx_data[NSERIAL];
-	Uart();
+	Uart(const char *);
 	bool sendUart(unsigned char* msg);
 	void readUart();
 	void closeUart();
