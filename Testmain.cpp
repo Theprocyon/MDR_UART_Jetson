008d1b95c32fa48285abb2c8b2db4ff9807c5de7
Theprocyon <46133206+Theprocyon@users.noreply.github.com>
2020-12-26 18:00:06 +0900

    Update Testmain.cpp

--- Testmain.cpp
+++ Testmain.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-	Uart Test("/dev/pts/4");
-	Test.sendUart('x');
-	int x;
-	cin >> x;
-	Test.readUart();
+	Uart JetsonNano("/dev/pts/4"); // please make UART data transfer loop using socat command
+	Uart Driver("/dev/pts/5"); // socat -d -d pty,raw,echo=0 pty,raw,echo=0
+	unsigned char Bytes[BUFFERSIZE] = {172,184,183,137,2,170,4,0}; //MDROBOT PROTOCOL - PID 137 SET BAUDRATE 
+	JetsonNano.sendUart(Bytes, 8);
+	Driver.readUart();
 	return 0;
 }
