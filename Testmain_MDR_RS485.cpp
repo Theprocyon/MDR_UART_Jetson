050ed7362dd7172a297d7fcac11d87f3fa90721c
Theprocyon <46133206+Theprocyon@users.noreply.github.com>
2021-01-16 00:03:53 +0900

    Update Testmain_MDR_RS485.cpp

--- Testmain_MDR_RS485.cpp
+++ Testmain_MDR_RS485.cpp
@@ -1,43 +1,20 @@
 #include "MD_COMM_UART.h"
+#define RPM 3600
 
-void PrintPacket(const PACKET& pct) {
-	for (auto i : pct)
-	printf("%d ", i);
-	printf("\n");
-	return;
-}
+
 int main() {
-	MDT MD750T_TRACK(nullptr, 1);
-	MDT MD750T_ARM(nullptr, 2);
-	//class MDT is Motercontroller of MDROBOT.
-	//Parameters of MDT(const char* UART PORT PATH,BYTE DEVICE ID)
-	MD750T_TRACK.Send_Signal(PID::ALARM_RESET, DONT_CARE);
 
-	//you can manually send signal any PID you want
-	MD750T_TRACK.Send_Signal(PID::ALARM_RESET, DONT_CARE);
-	MD750T_TRACK.Send_Signal(PID::STOP_STATUS, 1); //servo lock
+	MD750T TRACK(nullptr, 1);
+	MD750T ARM(nullptr, 2);
 
-	//in the case of the COMMAND pid, you can use PID10_CMD type as parameter.
-	MD750T_TRACK.Send_Signal(PID::COMMAND, PID10_CMD::CMD_POSI_RESET);
-	MD750T_TRACK.Send_Signal(PID::COMMAND, PID10_CMD::CMD_BRAKE);
-	MD750T_TRACK.Send_Signal(PID::COMMAND, PID10_CMD::CMD_ALARM_RESET);
+	//MOV_BY_POS는 상대위치 컨트롤.
+	TRACK.MOV_BY_POS(100, 100); //현재 위치로부터 100만큼 앞으로 이동
+	TRACK.MOV_BY_POS(-200, -200); //도착한 위치로부터 200만큼 뒤로 이동(초기로부터 -100)
+	TRACK.MOV_BY_POS(-1500, 1500); // 회전
 
-	//you can use just number as parameter (NOT RECOMMENDED)
-	MD750T_TRACK.Send_Signal((PID)10, (BYTE)10);
+	//MOV_BY_POS_ABS는 절대 위치 컨트롤. 
+	ARM.MOV_BY_POS_ABS(100, RPM, 100, RPM); //로봇팔을 100에 위치시킴(각도는 계산필요)
+	ARM.MOV_BY_POS_ABS(-200, RPM, -200, RPM); //로봇팔을 -200에 위치시킴(각도는 계산필요)
 
-	//Control code, pid 206 receives struct POSI_VEL_CMD
-	POSI_VEL_CMD Position;
-
-	Position.ID1ENABLE = 1;
-	Position.ID1MAXSPD = 100;
-	Position.ID1POS = 1024;
-	Position.ID2ENABLE = 1;
-	Position.ID2MAXSPD = 100;
-	Position.ID2POS = 1024;
-	Position.PID_MONITOR_ID = RETURN_PNT_MAIN_DATA;
-
-	MD750T_TRACK.Send_Signal(PID::PNT_POSI_VEL_CMD, Position); //position control sent
-	Position.ID1POS += 1024;
-	MD750T_TRACK.Send_Signal(PID::PNT_POSI_VEL_CMD, Position); //position control(motor 1's position += 1024) sent
 	return 0;
 }
