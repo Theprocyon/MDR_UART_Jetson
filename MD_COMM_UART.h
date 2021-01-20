ad994980781bbb6c35b410119431b4582f0a9235
Theprocyon <46133206+Theprocyon@users.noreply.github.com>
2021-01-21 00:13:19 +0900

    Update MD_COMM_UART.h

--- MD_COMM_UART.h
+++ MD_COMM_UART.h
@@ -1,151 +1,126 @@
-#include "MD_COMM_UART.h"
+#ifndef MD_COMM_UART
+#define MD_COMM_UART
+#include<vector>
+#include<iostream>
+#include"UART.h"
+//define constant IDs
+#define ENABLE                        1  
+#define RETURN_PNT_MAIN_DATA          2   
+#define MAX_PACKET_SIZE               30
+#define MOTOR_CONTROLLER_MACHINE_ID   184
+#define USER_MACHINE_ID               172
+#define BROADCAST_ID                  254
+#define DEFAULT_WRITE_CHK		      0x55
+#define WRITE_CHK		              0xaa
+#define DONT_CARE                     0x00
+//PIDs
+enum class PID {
+	//1byte data
+	VER = 1, //R
+	DEFAULT_SET = 3,
+	REQ_PID_DATA = 4,
+	ACK = 7, //R
+	COMMAND = 10,
+	ALARM_RESET = 12,
+	POSI_RESET = 13,
+	STOP_STATUS = 24, //RW
+	IN_POSITION_OK = 49,//R
 
-int Byte2Int(BYTE byLow, BYTE byHigh)
-{
-	return (byLow | (int)byHigh << 8);
-}
-long Byte2Long(BYTE byData1, BYTE byData2, BYTE byData3, BYTE byData4)
-{
-	return ((long)byData1 | (long)byData2 << 8 | (long)byData3 << 16 |
-		(long)byData4 << 24);
-}
-IBYTE Int2Byte(int nIn)
-{
-	IBYTE Ret;
+	//2byte data
+	VOLT_IN = 143, //R
+	RETURN_TYPE = 149, //RW
+	IN_POSITION = 171, //R
+	PNT_TQ_OFF = 174,
+	PNT_BRAKE = 175,
+	TAR_POSI_VEL = 176,
+	MONITOR1 = 196, //R
+	MONITOR2 = 201, //R
 
-	Ret.byLow = nIn & 0xff;
-	Ret.byHigh = nIn >> 8 & 0xff;
-	return Ret;
-}
-LBYTE Long2Byte(long lIn)
-{
-	LBYTE Ret;
+	//CTRLCODE
+	PNT_POSI_VEL_CMD = 206,
+	PNT_VEL_CMD = 207,
+	PNT_TQ_CMD = 209,
+	PNT_MAIN_DATA = 210, //R
+	PNT_INC_POSI_CMD = 215,
+	//Change the value of current position
 
-	Ret.byData1 = lIn & 0xff;
-	Ret.byData2 = lIn >> 8 & 0xff;
-	Ret.byData3 = lIn >> 16 & 0xff;
-	Ret.byData4 = lIn >> 24 & 0xff;
-	return Ret;
-}
+	POSI_SET1 = 217,
+	POSI_SET2 = 218,
+	MAX_RPM = 221 //RW
+};
 
-bool MD750T::TxPacket(const PACKET& pct) //incomplete
-{	
-	bool send_ok = uart->sendUart(pct.data() ,pct.size());
-	if(send_ok){
-		printf("Packet Sent. (ID : %d, PID : %d)\n",pct[2],pct[3]);
-	}else{
-		printf("Failed to send packet.\n");
-	}
-	return send_ok;
-}
+//Databytes of Command (PID10)
+enum class PID10_CMD {
+	CMD_TQ_OFF = 2,
+	CMD_BRAKE = 4,
+	MAIN_DATA_BC_ON = 5,
+	MAIN_DATA_BC_OFF = 6,
+	CMD_ALARM_RESET = 8,
+	CMD_POSI_RESET = 10,
+	CMD_EMER_ON = 67,
+	CMD_EMER_OFF = 68,
+};
 
-bool MD750T::RxPacket(void) //incomplete
-{
-	return false;
-}
+//define data structure; BYTE WORD
+typedef unsigned char 	   BYTE;
+typedef unsigned int 	   WORD;
+//PACKET and DATA is vector of BYTE
+typedef std::vector<BYTE>  PACKET;
+typedef std::vector<BYTE>  DATA;
+//INT(16bit) for 8bit CPU
+typedef struct {
+	BYTE byLow;
+	BYTE byHigh;
+} IBYTE;
+//LONG(32bit) for 8bit CPU
+typedef struct {
+	BYTE byData1;	// Low Byte
+	BYTE byData2;
+	BYTE byData3;
+	BYTE byData4;	// High Byte
+} LBYTE;
 
-BYTE MD750T::CalcChecksum(const PACKET& pkt)
-{
-	BYTE chksent = 0b00000000;
-	for (auto i : pkt) chksent += i;
-	return (~chksent) + 1;
-}
+// define BYTE - INT - LONG converter
+int Byte2Int(BYTE, BYTE);
+long Byte2Long(BYTE, BYTE, BYTE, BYTE);
+IBYTE Int2Byte(int);
+LBYTE Long2Byte(long);
 
+//define ctrlstruct to data
+struct STATE {
+	int  ID1RPM = 0;
+	int  ID1AMP = 0;
+	BYTE   ID1STATE = 0;
+	long  ID1POS = 0;
 
-PACKET MD750T::StructPacket(BYTE pid, const DATA& data)
-{
-	PACKET Packet;
-	Packet.push_back(MOTOR_CONTROLLER_MACHINE_ID); //RMID
-	Packet.push_back(USER_MACHINE_ID);             //PMID
-	Packet.push_back(this->DEVICE_ID);             //DEV_ID
-	Packet.push_back(pid);                         //PID
-	Packet.push_back(data.size());                 //DataNumber
-	for (auto i : data) 	Packet.push_back(i);   //DATA
-	Packet.push_back(this->CalcChecksum(Packet));  //CHK
-	return Packet;
-}
+	int  ID2RPM = 0;
+	int  ID2AMP = 0;
+	BYTE   ID2STATE = 0;
+	long  ID2POS = 0;
+};
 
-MD750T::MD750T(Uart* _uart, BYTE id)
-{
-	if (id > 0 || id < 254) {
-		this->DEVICE_ID = id;
-		printf("MD_COMM.cpp : Device id %d constructed.\n",this->DEVICE_ID);
-	}else{
-		printf("MD_COMM.cpp : Failed to construct MD750T, Wrong device id.\n");
-	}
-	this->uart = _uart;
-}
+class MD750T {
+private:
+	Uart*     uart;
+	BYTE      DEVICE_ID = BROADCAST_ID;
+	STATE     DEVICE_STATE = { 0 };
+	bool      TxPacket(const PACKET&);
+	bool      RxPacket(void);
+	BYTE      CalcChecksum(const PACKET&);
+	PACKET    StructPacket(BYTE pid, const DATA&);
 
-bool MD750T::Send_Signal(PID pid, const DATA& data) {
-	return (this->TxPacket(PACKET(StructPacket((BYTE)pid, data))));
-}
+public:
 
-bool MD750T::Send_Signal(PID pid, BYTE _data_1)
-{
-	DATA data(1, _data_1);
-	return (this->TxPacket(PACKET(StructPacket((BYTE)pid, data))));
-}
+	//lowlevel control
+	MD750T(Uart*, BYTE);
+	bool Send_Signal(PID pid, const DATA&);
+	bool Send_Signal(PID pid, BYTE);
+	bool Send_Signal(PID pid, BYTE, BYTE);
+	bool Send_Signal(PID10_CMD);
+	
+	//highlevel control
+	bool MOV_BY_POS(long, long);
+	bool MOV_BY_POS_ABS(long, int, long, int);
+};
 
-bool MD750T::Send_Signal(PID pid, BYTE _data_1, BYTE _data_2)
-{
-	DATA data;
-	data.push_back(_data_1);
-	data.push_back(_data_2);
-	return (this->TxPacket(PACKET(StructPacket((BYTE)pid, data))));
-}
-
-
-bool MD750T::Send_Signal(PID10_CMD cmd)
-{
-	DATA data(1, (BYTE)cmd);
-	return (this->TxPacket(PACKET(StructPacket(10, data))));
-}
-
-bool MD750T::MOV_BY_POS(long id1pos, long id2pos)
-{
-	DATA data(11);
-	LBYTE _id1_pos(Long2Byte(id1pos));
-	LBYTE _id2_pos(Long2Byte(id2pos));
-
-	data[0] = (id1pos == 0) ? 0 : 1;
-	data[1] = _id1_pos.byData1;
-	data[2] = _id1_pos.byData2;
-	data[3] = _id1_pos.byData3;
-	data[4] = _id1_pos.byData4;
-	data[5] = (id2pos == 0) ? 0 : 1;
-	data[6] = _id2_pos.byData1;
-	data[7] = _id2_pos.byData2;
-	data[8] = _id2_pos.byData3;
-	data[9] = _id2_pos.byData4;
-	data[10] = RETURN_PNT_MAIN_DATA;
-
-	return Send_Signal(PID::PNT_INC_POSI_CMD, data);
-}
-
-bool MD750T::MOV_BY_POS_ABS(long id1pos, int id1spd, long id2pos, int id2spd)
-{
-	DATA data(15);
-
-	IBYTE _id1_spd(Int2Byte(id1spd));
-	IBYTE _id2_spd(Int2Byte(id2spd));
-	LBYTE _id1_pos(Long2Byte(id1pos));
-	LBYTE _id2_pos(Long2Byte(id2pos));
-
-	data[0] = ENABLE;
-	data[1] = _id1_pos.byData1;
-	data[2] = _id1_pos.byData2;
-	data[3] = _id1_pos.byData3;
-	data[4] = _id1_pos.byData4;
-	data[5] = _id1_spd.byLow;
-	data[6] = _id1_spd.byHigh;
-	data[7] = ENABLE;
-	data[8] = _id2_pos.byData1;
-	data[9] = _id2_pos.byData2;
-	data[10] = _id2_pos.byData3;
-	data[11] = _id2_pos.byData4;
-	data[12] = _id2_spd.byLow;
-	data[13] = _id2_spd.byHigh;
-	data[14] = RETURN_PNT_MAIN_DATA;
-
-	return Send_Signal(PID::PNT_POSI_VEL_CMD, data);
-}
+#endif
